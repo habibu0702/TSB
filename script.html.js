30f9d3e364873fad6996291b2d7e6d8b41683ca6
TSB <habibusulaiman287@gmail.com>
2024-11-29 12:19:30 +0100

    Add files via upload

---
+++ script.html.js
@@ -0,0 +1,30 @@
+
+
+document.getElementById('receive').addEventListener('click', function() {
+    alert('stay tuned, coming soon');
+});
+
+document.getElementById('send').addEventListener('click', function() {
+    alert('coming soon');
+    alert('hello habibu');
+});
+
+document.getElementById('swap').addEventListener('click', function() {
+    alert('stay tuned, coming soon');
+});
+
+document.getElementById('choose').addEventListener('click', function() {
+    alert('kai lukman you are very stopet');
+
+});
+
+
+
+
+document.getElementByI('open-file').addEventListener
+('click', function() {
+    openFile
+});
+function openFile(aidrop) {
+    window.open(aidrop, '_blank');
+}
